5633330c98f86d14909b5fe0261afc329c00c411
randomblue <jpmedallon@gmail.com>
2018-07-11 19:38:49 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        My Name is JP
+      </div>
+    );
+  }
+}
+
+const Header = () => {
+  return <header>I am a header</header>
+}
+
+
+
+export default App;
